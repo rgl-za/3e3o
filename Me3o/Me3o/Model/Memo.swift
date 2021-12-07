ff0a40bdbeeb5fb33718e343f73d95828ad223fa
Hyeonjeong <76260153+rgl-za@users.noreply.github.com>
2021-12-07 18:26:13 +0900

    메모 모델 클래스 주석 정리

--- Me3o/Me3o/Model/Memo.swift
+++ Me3o/Me3o/Model/Memo.swift
@@ -8,9 +8,11 @@
 import SwiftUI
 
 class Memo: Identifiable, ObservableObject{
-    let id: UUID
-    @Published var content: String
-    let insertDate: Date
+    // Identifiable: 데이터 목록을 테이블 뷰나 컬렉션 뷰에 쉽게 바인딩 할 수 있음
+    // ObservableObject: 반응형 UI를 구현하기 위함
+    let id: UUID // Identifiable 프로토콜이 요구하는 속성, 메모를 유일하게 구분
+    @Published var content: String // 메모 내용 저장: Published 특성을 추가하여 새로운 값이 저장될 때마다 바인딩 되어있는 UI가 자동 업데이트
+    let insertDate: Date // 메모 생성 날짜 저장
     
     // 초기화 생성자
     init(id: UUID = UUID(), content: String, insertDate: Date = Date()){
@@ -20,7 +22,7 @@ class Memo: Identifiable, ObservableObject{
     }
 }
 
-extension Memo: Equatable {
+extension Memo: Equatable { 
     static func == (lhs: Memo, rhs: Memo)-> Bool{
         return lhs.id == rhs.id
     }
